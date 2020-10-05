bba55c71dadf998432d58f024e03f316590fa021
Jmart79 <justinjustinjust111@gmail.com>
2020-10-05 09:24:58 -0600

    Initial Creation

---
+++ LaFinca/LaFinca/LaFinca/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using LaFinca.ViewModels;
+
+namespace LaFinca.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
