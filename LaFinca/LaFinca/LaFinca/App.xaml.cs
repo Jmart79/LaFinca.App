2997c99c3e482ce649bcb5072f1b89141b82be9a
Jmart79 <justinjustinjust111@gmail.com>
2020-10-07 09:32:48 -0600

    Not sure what to label this
    accidentaly a big commit

--- LaFinca/LaFinca/LaFinca/App.xaml.cs
+++ LaFinca/LaFinca/LaFinca/App.xaml.cs
@@ -14,7 +14,7 @@ namespace LaFinca
             InitializeComponent();
 
             DependencyService.Register<MockDataStore>();
-            MainPage = new NavigationPage(new HomePage());
+            MainPage = new NavigationPage(new NewMenuItemPage());
         }
 
         protected override void OnStart()
--- LaFinca/LaFinca/LaFinca/Views/SearchPagexaml.xaml.cs
+++ LaFinca/LaFinca/LaFinca/Views/SearchPagexaml.xaml.cs
@@ -10,9 +10,9 @@ using Xamarin.Forms.Xaml;
 namespace LaFinca.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class TabbedPage1 : TabbedPage
+    public partial class SearchPagexaml : TabbedPage
     {
-        public TabbedPage1()
+        public SearchPagexaml()
         {
             InitializeComponent();
         }
