5d603ac74f74b044367232c1d8ae41259eabcccc
Jmart79 <justinjustinjust111@gmail.com>
2020-10-13 19:12:01 -0600

    Fixing up update page

--- LaFinca/LaFinca/LaFinca/Views/UpdateMenuItemPage.xaml.cs
+++ LaFinca/LaFinca/LaFinca/Views/UpdateMenuItemPage.xaml.cs
@@ -25,47 +25,6 @@ namespace LaFinca.Views
 
         }
 
-        private void IsHouseFavoriteSwitch1_Toggled(object sender, ToggledEventArgs e)
-        {
-
-        }
-
-        private void Picker_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            string VariableToChange = VariableToUpdatePicker.SelectedItem.ToString().ToLower();
-
-            switch (VariableToChange)
-            {
-                case "description":
-                    GenerateEntry("Description");
-                    break;
-
-                case "cost":
-                    break;
-                case "category":
-                    break;
-                case "IsAvailable":
-                    break;
-                case "IsHouseFavorite":
-                    break;
-                case "ItemName":
-                    break;
-                default:
-                    break;
-            }
-
-        }
-
-        private void GenerateEntry(string placeholder)
-        {
-            Entry entry = new Entry();
-
-            entry.Placeholder = placeholder;
-            //set binding context to a new user
-            //set event handler to a generalized event hanlder to handle all entries that might be created for updates.
-            //
-        }
-
         private void ItemToUpdate_Completed(object sender, EventArgs e)
         {
              ItemToUpdateName = ItemToUpdate.Text;
